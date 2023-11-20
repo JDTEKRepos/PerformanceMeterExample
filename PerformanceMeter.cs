145170c9e09ceb439ec1508b281fd73821acc802
HyeongRyong <142376090+HyeongRyong@users.noreply.github.com>
2023-11-20 10:46:28 +0900

    초기 커밋.

---
+++ PerformanceMeter.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+
+namespace PerformanceMeterExample;
+
+public class PerformanceMeter
+{
+    /// <summary>
+    /// 동기 방식의 반환 값이 없는 void형 메서드의 성능을 측정
+    /// </summary>
+    /// <param name="action">성능을 측정할 메소드</param>
+    /// <returns>수행된 작업의 경과 시간</returns>
+    public static TimeSpan Check(Action action)
+    {
+        Stopwatch watch = Stopwatch.StartNew();
+        action();
+        watch.Stop();
+
+        return watch.Elapsed;
+    }
+
+    /// <summary>
+    /// 동기 방식의 반환 값이 있는 메서드의 성능을 측정
+    /// </summary>
+    /// <typeparam name="T">반환 값의 타입</typeparam>
+    /// <param name="func">성능을 측정할 Func 메소드</param>
+    /// <returns>수행된 작업의 경과 시간</returns>
+    public static TimeSpan Check<T>(Func<T> func)
+    {
+        Stopwatch watch = Stopwatch.StartNew();
+        func();
+        watch.Stop();
+
+        return watch.Elapsed;
+    }
+
+    /// <summary>
+    /// 비동기 방식의 메서드의 성능을 측정
+    /// </summary>
+    /// <param name="func">성능을 측정할 비동기 메소드</param>
+    /// <returns>비동기 작업 완료 후 경과 시간</returns>
+    public static async Task<TimeSpan> CheckAsync(Func<Task> func)
+    {
+        Stopwatch watch = Stopwatch.StartNew();
+        await func();
+        watch.Stop();
+
+        return watch.Elapsed;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿namespace PerformanceMeterExample;
+
+public class Program
+{
+    /// <summary>
+    /// 동기 방식의 메소드 측정 에제
+    /// </summary>
+    static void Main()
+    {
+        Console.WriteLine("============= start ==============");
+
+        var elapsed = PerformanceMeter.Check(() => PerformanceTimerExample(10));
+        Console.WriteLine($"ActionType Time elapsed: {elapsed}");
+
+        // 반환 값이 있는 메소드 측정 방법
+        var funcElapsed = PerformanceMeter.Check(() => PerformanceTimerFuncExample(5));
+        Console.WriteLine($"FuncType Time elapsed: {funcElapsed}");
+
+        Console.WriteLine("=============  end  ==============");
+
+        Console.ReadLine();
+    }
+
+    private static void PerformanceTimerExample(int maxValue)
+    {
+        double n = 0;
+        for (int i = 0; i < maxValue; i++)
+        {
+            n *= 2;
+
+            Thread.Sleep(100);
+        }
+    }
+
+    private static double PerformanceTimerFuncExample(int maxValue) 
+    {
+        double n = 0;
+        for (int i = 0; i < maxValue; i++)
+        {
+            n *= 2;
+
+            Thread.Sleep(100);
+        }
+
+        return n;
+    }
+
+    #region 비동기 방식 측정 예제
+    /*
+     
+    /// <summary>
+    /// 비동기 방식의 메소드 측정 예제
+    /// </summary>
+    static async Task Main()
+    {
+        Console.WriteLine("============= start ==============");
+
+        var asyncElapsed = await PerformanceMeter.CheckAsync(async () => await PerformanceTimerExample(13));
+        Console.WriteLine($"Time elapsed: {asyncElapsed}");
+
+        Console.WriteLine("=============  end  ==============");
+
+        Console.ReadLine();
+    }
+
+    private static async Task PerformanceTimerExample(int maxValue)
+    {
+        double n = 0;
+        for (int i = 0; i < maxValue; i++)
+        {
+            n *= 2;
+
+            await Task.Delay(100);
+        }
+    }
+
+    */
+    #endregion
+}
